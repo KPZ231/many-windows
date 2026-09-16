c5856d6ef268e39492e6dabf16318bd900486176
KPZ231 <kapieksperimental@gmail.com>
2026-09-16 21:59:05 +0200

    fix: correct floating window function implementation and update invoke handler

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,15 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
-use tauri::WebviewWindowBuilder;
+use tauri::{WebviewUrl, WebviewWindowBuilder};
+use tauri::{AppHandle, Manager};
 
 // Floating window function built in Rust
 #[tauri::command]
-fn floatingWindow(app: AppHandle) -> tauri::Result<()>{
-    match app.webview_windows().get("floating"){
-        None =>{
-            WebviewWindowBuilder::new($app, "floating", tauri::WindowUrl::App("index.html".into()))
-                .alwats_on_top(true)
+fn floating(app: AppHandle) -> tauri::Result<()> {
+    match app.webview_windows().get("floating") {
+        None => {
+            WebviewWindowBuilder::new(&app, "floating", WebviewUrl::App("index.html".into()))
+                .always_on_top(true)
                 .decorations(false)
                 .inner_size(400.0, 400.0)
                 .position(0.0, 0.0)
@@ -26,7 +27,7 @@ fn floatingWindow(app: AppHandle) -> tauri::Result<()>{
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![floating])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
